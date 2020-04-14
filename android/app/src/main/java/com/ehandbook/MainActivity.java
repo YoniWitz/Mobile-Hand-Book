b92a396d7f72d2f637b22b8b1106de20909a1782
Yoni Hirshowitz <jonhirsh39@gmail.com>
2020-04-14 17:39:59 -0400

    updated project name and published to testing

--- android/app/src/main/java/com/ehandbook/MainActivity.java
+++ android/app/src/main/java/com/ehandbook/MainActivity.java
@@ -1,4 +1,4 @@
-package com.newmobilehandbook;
+package com.ehandbook;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "NewMobileHandBook";
+    return "EHandBook";
   }
 }
